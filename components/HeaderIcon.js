f260427a3e14840d28de407bc1a3bf4c778eb0bd
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-02-12 14:44:39 +0700

    implemanting next-auth for this fb-clone

--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -1,3 +1,4 @@
+    
 function HeaderIcon({ Icon, active }) {
     return (
         <div className="flex items-center cursor-pointer md:px-10 sm:h-14 md:hover:bg-gray-100 rounded-xl active:border-b-2 active:border-blue-500 group">
--- components/Login.js
+++ components/Login.js
@@ -2,17 +2,17 @@ import Image from 'next/image';
 
 function Login() {
     return (
-        <div>
+        <div className="grid place-items-center">
         <Image 
             src="https://links.papareact.com/t4i"
             height={400}
             width={400}
-            objectFit="contained"
+            objectFit="contain"
             alt="Login"
         />
-
+        <h1 className="p-5 bg-blue-500 rounded-full text-white text-center cursor-pointer">Login with Facebook</h1>
         </div>
-    )
+    );
 }
 
-export default Login();
+export default Login;
