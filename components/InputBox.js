ad5e30e6ec238fefb6abf8f6a73615c19c42acb0
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-02-12 22:57:41 +0700

    added firebase

--- components/InputBox.js
+++ components/InputBox.js
@@ -1,12 +1,16 @@
 import Image from 'next/image'
-import { session } from 'next-auth/client';
 import { useSession } from 'next-auth/client';
+import { EmojiHappyIcon } from '@heroicons/react/outline';
+import { CameraIcon, VideoCameraIcon } from '@heroicons/react/solid';
 
 function InputBox() {
     const [session] = useSession();
+    const sendPost = (e) => {
+        e.preventDefault();
+    };
 
     return (
-        <div>
+        <div className="bg-white p-2 rounded-2xl shadow-md text-gray-500 font-medium mt-6">
             <div className="flex space-x-4 p-4 items-center">
                 <Image 
                     className="rounded-full"
@@ -19,9 +23,28 @@ function InputBox() {
                     <input
                         className="rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none" 
                         type="text"
-                        placeholder={`What on you mind, ${session.user.name}`}
+                        placeholder={`What on you mind, ${session.user.name}?`}
                 />
                 </form>
+                <button hidden type="submit" onClick={sendPost}>Submit</button>
+            </div>
+
+            {/* This div for all icon in Input components */}
+            <div className="flex justify-evenly p-3 border-t">
+                <div className="inputIcon">
+                    <VideoCameraIcon className="h-7 text-red-500" />
+                    <p className="text-xs sm:text-sm xl:text-base">Live Video</p>
+                </div>
+
+                <div className="inputIcon">
+                    <CameraIcon className="h-7 text-green-400" />
+                    <p className="text-xs sm:text-sm xl:text-base">Photo/Video</p>
+                </div>
+
+                <div className="inputIcon">
+                    <EmojiHappyIcon className="h-7 text-yellow-300" />
+                    <p className="text-xs sm:text-sm xl:text-base">Fealing/Activity</p>
+                </div>
             </div>
         </div>
     )
---
+++ firebase.js
@@ -0,0 +1,11 @@
+import firebase from './firebase';
+import 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCnw5eQ0g4LPJm95qTlWndEXYZqcbCZvAU",
+    authDomain: "facebook-clone-91e54.firebaseapp.com",
+    projectId: "facebook-clone-91e54",
+    storageBucket: "facebook-clone-91e54.appspot.com",
+    messagingSenderId: "678096756463",
+    appId: "1:678096756463:web:058e06bdfcb31f2c4c451a"
+  }; 
