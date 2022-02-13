c860d78314c88128ea04c51215b8ba5b7693edd4
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-02-13 13:42:47 +0700

    Building the inputBox components

--- components/InputBox.js
+++ components/InputBox.js
@@ -1,6 +1,6 @@
 import { useRef, useState } from 'react';
 import Image from 'next/image';
-import { db } from '../firebase';
+import { db, storage } from '../firebase';
 import firebase from 'firebase';
 import { useSession } from 'next-auth/client';
 import { EmojiHappyIcon } from '@heroicons/react/outline';
@@ -11,7 +11,7 @@ function InputBox() {
     const [session] = useSession();
     const inputRef = useRef(null);
     const filepickerRef = useRef(null);
-    const [imagePost, setImagePost] = useState(null);
+    const [imageToPost, setImageToPost] = useState(null);
 
     /* This function for create data to firebase */
     const sendPost = (e) => {
@@ -25,6 +25,30 @@ function InputBox() {
             email: session.user.email,
             image: session.user.image,
             timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        }).then(doc => {
+            if (imageToPost) {
+                // funcky upload stuff for image
+                const uploadTask = storage.ref(`posts/${doc.id}`).putString(imageToPost, 'data_url');
+
+                removeImage();
+
+                uploadTask.on(
+                    'state_changed',
+                    null,
+                    (error) => console.error(error),
+                    () => {
+                    // When the upload completes
+                        storage.ref('posts').child(doc.id).getDownloadURL().then(url => {
+                            db.collection('posts').doc(doc.id).set(
+                            {
+                                postImage: url
+                            }, 
+                                { merge: true }
+                            );
+                        });
+                    }
+                );
+            }
         });
 
         inputRef.current.value = "";
@@ -32,8 +56,19 @@ function InputBox() {
 
     /* This function for add image to post */
     const addImageToPost = (e) => {
+        const reader = new FileReader();
+        if (e.target.files[0]) {
+            reader.readAsDataURL(e.target.files[0]);
+        }
 
-    }
+        reader.onload = (readerEvent) => {
+            setImageToPost(readerEvent.target.result);
+        };
+    };
+
+    const removeImage = () => {
+        setImageToPost(null);
+    };
 
     return (
         <div className="bg-white p-2 rounded-2xl shadow-md text-gray-500 font-medium mt-6">
@@ -54,6 +89,21 @@ function InputBox() {
                     />
                 <button hidden type="submit" onClick={sendPost}>Submit</button>
                 </form>
+
+                {/* this to viewing in inputbox */}
+                {imageToPost && (
+                    <div 
+                        onClick={removeImage}
+                        className="flex flex-col filter hover:brightness-110 transition duration-150 transform hover:scale-150 cursor-pointer"
+                    >
+                        <img 
+                            className="h-10 object-contain"
+                            src={imageToPost} 
+                            alt="Post Image" 
+                        />
+                        <p className="text-xs text-red-500 text-center">Remove</p>
+                    </div>
+                )}
             </div>
 
             {/* This div for all icon in Input components */}
@@ -72,7 +122,7 @@ function InputBox() {
                     <input 
                         ref={filepickerRef} 
                         onChange={addImageToPost} 
-                        type="text"
+                        type="file"
                         hidden 
                     />
                 </div>
